ff4e6ec6f7c4e7a2eea7316c1b3212826090351c
Thomas Bille <contact@tbille.fr>
2017-09-17 10:05:18 +0200

    Add material-ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'roboto-npm-webfont';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
